e130f7eafa16c5d7d8516805e5f25543733595bd
karanchopras <karanchopras@gmail.com>
2023-06-13 16:50:57 +0200

    v1

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -17,7 +17,7 @@ module.exports = (srv) => {
   
     // Add some discount for overstocked books
     srv.after ('READ', 'Books', each => {
-      if (each.stock > 111)  each.title += ' -- 12% discount!'
+      if (each.stock > 111)  each.title += ' -- 11 discount!'
     }) 
   
   }
